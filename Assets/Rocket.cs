342382658f7444168ac078e28100c8b255e73a3d
Justin King <justin@dapshow.com>
2019-04-06 14:11:30 -0700

    add sounds to rocket boost

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource rocketSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,15 +26,23 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketSound.isPlaying)
+            {
+                rocketSound.Play();
+            }
+        }
+        else
+        {
+            rocketSound.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating Left!");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right!");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
